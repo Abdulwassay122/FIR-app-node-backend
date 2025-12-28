c1b4e946d8a5a4b05226bd02fdb72cd1ca9f568c
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2025-12-28 18:54:00 +0500

    for render

--- src/index.js
+++ src/index.js
@@ -1,61 +1,18 @@
-// import dotenv from "dotenv";
-// // import { app } from "./app.js";
-// import sequelize from "./config/db.js";
-// import express from "express";
-
-// dotenv.config({
-//   path: "./.env",
-// });
-// const app = express();
-// const port = process.env.PORT || 3000;
-
-// app.use(express.json());
-
-// // Example route
-// app.get("/", (req, res) => {
-//   res.send("MongoDB + Express Connected!");
-// });
-
-// await sequelize
-//   .authenticate()
-//   .then(() => {
-//     app.listen(port, "0.0.0.0", () => {
-//       console.log(`✅ Server running on port ${port}`);
-//     });
-//   })
-//   .catch((err) => {
-//     console.log("Neon posgre connection failed !!! ", err);
-//   });
-
-// // for vercel
 import dotenv from "dotenv";
-// import { app } from "./app.js";
+import { app } from "./app.js";
 import sequelize from "./config/db.js";
-
-import express from "express";
 
 dotenv.config({
   path: "./.env",
 });
-const app = express();
 
-let dbConnected = false;
-
-export default async function handler(req, res) {
-  try {
-    if (!dbConnected) {
-      await sequelize.authenticate();
-      console.log("Database connected!");
-      dbConnected = true;
-    }
-
-    app(req, res);
-  } catch (err) {
-    console.error("Serverless function error:", err);
-    res.status(500).json({
-      success: false,
-      message: "Serverless function crashed",
-      error: err.message,
+await sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
     });
-  }
-}
+  })
+  .catch((err) => {
+    console.log("SQL connection failed !!! ", err);
+  });
