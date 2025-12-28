0dded7314a7ecd0047c8e79a1dd78a897b47d95e
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2025-12-28 19:09:27 +0500

    test for vercel 08

--- src/index.js
+++ src/index.js
@@ -6,13 +6,24 @@ dotenv.config({
   path: "./.env",
 });
 
-await sequelize
-  .sync()
-  .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log("SQL connection failed !!! ", err);
-  });
+// await sequelize
+//   .sync()
+//   .then(() => {
+
+app.get("/", (req, res) => {
+  res.send("Server is running!");
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running at port : ${process.env.PORT}`);
+});
+
+try {
+  await sequelize.sync();
+} catch (error) {
+  console.log(error);
+}
+// })
+// .catch((err) => {
+//   console.log("SQL connection failed !!! ", err);
+// });
