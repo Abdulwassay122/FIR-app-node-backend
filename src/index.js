dca70abc9edd7adebde9257de522aec7b71e9849
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2025-12-28 19:17:42 +0500

    twst 13

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
-import { app } from "./app.js";
+// import { app } from "./app.js";
 // import sequelize from "./config/db.js";
+import express from "express";
 
 dotenv.config({
   path: "./.env",
@@ -9,7 +10,21 @@ dotenv.config({
 // await sequelize
 //   .sync()
 //   .then(() => {
+const app = express();
 
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(process.env.POSTGRES_URI, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
+await sequelize.sync();
 app.get("/", (req, res) => {
   res.send("Server is running!");
 });
