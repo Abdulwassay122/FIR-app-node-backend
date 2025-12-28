e7cf8521d854fb203f8db2a2faa6eac6b5c8e4cc
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2025-12-28 17:03:20 +0500

    test dep 02

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import { app } from "./app.js";
-import sequelize from "./config/db.js";
+// import { app } from "./app.js";
+// import sequelize from "./config/db.js";
 
 dotenv.config({
   path: "./.env",
