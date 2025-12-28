b3d06df3a2db0d5991f1f75d2a4ef6e666b9a232
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2025-12-28 19:27:31 +0500

    test 14

--- src/config/db.js
+++ src/config/db.js
@@ -2,12 +2,12 @@ import { Sequelize } from "sequelize";
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
   dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+  // dialectOptions: {
+  //   ssl: {
+  //     require: true,
+  //     rejectUnauthorized: false,
+  //   },
+  // },
   logging: false,
 });
 
--- src/index.js
+++ src/index.js
@@ -1,44 +1,20 @@
 import dotenv from "dotenv";
 // import { app } from "./app.js";
-// import sequelize from "./config/db.js";
+import sequelize from "./config/db.js";
 import express from "express";
+const app = express();
 
 dotenv.config({
   path: "./.env",
 });
 
-// await sequelize
-//   .sync()
-//   .then(() => {
-const app = express();
-
-import { Sequelize } from "sequelize";
-
-const sequelize = new Sequelize(process.env.POSTGRES_URI, {
-  dialect: "postgres",
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
-  logging: false,
-});
-await sequelize.sync();
-app.get("/", (req, res) => {
-  res.send("Server is running!");
-});
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is running at port : ${process.env.PORT}`);
-});
-
-// try {
-//   await sequelize.sync();
-// } catch (error) {
-//   console.log(error);
-// }
-// })
-// .catch((err) => {
-//   console.log("SQL connection failed !!! ", err);
-// });
+await sequelize
+  .sync()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("SQL connection failed !!! ", err);
+  });
